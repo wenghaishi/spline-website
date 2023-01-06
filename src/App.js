bf3d0eba9e3a412ae82d71479fe007988adfaea0
sam <wenghaishi1@gmail.com>
2023-01-06 18:47:45 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import SplineComp from './components/SplineComp';
+
+function App() {
+  return (
+    <div className='App'>
+      <SplineComp />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SplineComp.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './SplineComp.css'
+import Spline from '@splinetool/react-spline';
+
+
+function SplineComp() {
+  return (
+    <div className='main'>
+        <Spline scene="https://prod.spline.design/HWPnd7hxKtoS2opx/scene.splinecode" />
+    </div>
+  )
+}
+
+export default SplineComp
+
